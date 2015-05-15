50eb5a5e11b85c6b1f74aa8598703ecc83df0c46
Seth Hendrick <seth@shendrick.net>
2015-05-14 21:35:05 -0400

    Made progress on Generate.py, but need to be on the server.

--- src/Generate.py
+++ src/Generate.py
@@ -5,14 +5,46 @@
  http://www.boost.org/LICENSE_1_0.txt)
 '''
 import argparse
+from mysql.connector import (connection)
 import os
 
-def Generate( fileToExport ):
-    print( fileToExport )
+from Configure import *
+
+class Generator():
+    def __init__(self, outFile, settings):
+        self.outFile = outFile
+        self.settings = settings
+        self.db = connection.MySQLConnection(
+            user = self.settings['DB_USERNAME'],
+            password = self.settings['DB_PASSWORD'],
+            host = self.settings['DB_URL'],
+            database = self.settings['DB_NAME']
+        )
+        self.curser = None
+
+    def __enter__(self):
+        return self
+
+    def GetRandomItem(self):
+        query = "SELECT * FROM %s" + \
+            " WHERE shown=0 ORDER BY RAND() LIMIT 1"
+        
+        cursor = self.database.cursor()
+        cursor.execute(query, (self.settings['DB_TABLE']))
+        print (cursor)
+
+    def __exit__(self, type, value, traceback):
+        if ( self.curser != None ):
+            curser.close()
+        
+        if ( self.db != None ):
+            db.close()
+        
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser( description="Reads a random something from the database and writes it to the given file." )
     argParser.add_argument("outfile", action = "store", default="index.txt", help="The file to export to.")
     args = argParser.parse_args()
     
-    Generate( args.outfile )
+    with Generator(args.outfile, SETTINGS) as generator:
+        generator.GetRandomItem()
