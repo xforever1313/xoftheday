32b4d01a66a1420e30f5950831e34e955097cdc6
Seth Hendrick <seth@shendrick.net>
2015-05-14 21:04:04 -0400

    Hello world for Generate.py

--- src/Generate.py
+++ src/Generate.py
@@ -4,3 +4,15 @@
  (See accompanying file ../LICENSE_1_0.txt or copy at
  http://www.boost.org/LICENSE_1_0.txt)
 '''
+import argparse
+import os
+
+def Generate( fileToExport ):
+    print( fileToExport )
+
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser( description="Reads a random something from the database and writes it to the given file." )
+    argParser.add_argument("outfile", action = "store", default="index.txt", help="The file to export to.")
+    args = argParser.parse_args()
+    
+    Generate( args.outfile )
